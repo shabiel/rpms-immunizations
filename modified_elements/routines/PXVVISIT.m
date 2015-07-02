aa82f0b866f0acfd0090da06b650dc96106e52da
Sam Habiel <sam.habiel@gmail.com>
2015-07-02 14:48:53 -0700

    Baseline for the new VISTA visits routine

---
+++ modified_elements/routines/PXVVISIT.m
@@ -0,0 +1,10 @@
+PXVVISIT ;VEN/SMH - Main VISTA Immunizations Visit Filer;7/2/2015
+ ;;0.0;IMMUNIZATIONS VISTA PORT;
+ ;
+ ; This file contains the main code for interacting with the VISTA
+ ; V files from the Ported RPMS Immunizations package
+ ;
+ ; The callers are BIVISIT and BIVISIT1
+ ;
+ ; The code here mirrors the RPMS code
+ ;
